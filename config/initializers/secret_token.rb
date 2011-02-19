f50c5f452a090a1ff3fc150bc58bf05cc3d78c73
lexer <lexer.box@gmail.com>
2011-02-19 17:07:37 +0500

    init rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ssap::Application.config.secret_token = 'e8cb641abb276592b7757b5cb46a9c7f250e293c32bab002f1e39440a09828d99278833f3cec7341aef680543420b369fadc926e3cfc784594d9dad921968ae2'
