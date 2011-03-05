8f2ab5cc44d9ffc6ad3a5b12968da1ebae8330a5
Alexey Zakharov <lexer.box@gmail.com>
2011-03-05 16:54:19 +0500

    add password action

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,4 +4,18 @@ class PasswordsController < ApplicationController
     @passwords = Password.all;
   end
 
+  def new
+    @password = Password.new
+  end
+
+  def create
+    @password = Password.new(params[:password])
+
+    if @password.save
+      redirect_to :action=>:index, :notice => "Password succefully added"
+    else
+
+      render :action => :new
+    end
+  end
 end
