b7c9e30ff4de004ebc3382256e2faf4d1e8f3fd0
oksana <utyasheva.oksana@gmail.com>
2011-03-05 16:13:54 +0500

    delete admin folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Ssap::Application.routes.draw do
   devise_for :users
-  root :to => "devise/sessions#new"
+  match "/" => "devise/sessions#new"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+     ``
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
