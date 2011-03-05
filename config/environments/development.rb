ce5b29e6d15bcfa95d9c613142b18575d60454f5
oksana <utyasheva.oksana@gmail.com>
2011-03-05 19:09:07 +0500

    fix devise passwords controller urls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,17 @@ Ssap::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+          :address => "smtp.gmail.com",
+          :port => "587",
+          :domain => "witcraft.ru",
+          :authentication => :plain,
+          :user_name => "test@witcraft.ru",
+          :password => "123456!"
+  }
+
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Ssap::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "ssap.heroku.com" }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
